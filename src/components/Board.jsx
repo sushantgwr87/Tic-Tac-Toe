03414ca654d0fb406094ef0901953e018f532e2f
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-28 10:11:30 +0530

    Updated Board Component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,34 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
 import MarkType from './MarkType';
+import Slot from '../partials/Slot';
 
 const Board = ({ mark, mode }) => {
 
+  const isCross = mark === "cross";
+
+  const [turnValue, setTurnValue] = useState("cross");
+
+  const handleClick = () => {
+    setTurnValue(turnValue === "cross" ? "circle" : "cross");
+  }
+
   return (
     <div className="board">
-      <div className="board_header">
-        <div className="board_mark_turn">
-          <MarkType name='cross' width='40px' />
-          <MarkType name='circle' width='40px' />
-        </div>
-        <div className="board_turn_btn">
-          <MarkType name='cross' fill='#dad8d8' width='20px' />
-          <span>Turn</span>
-        </div>
-        <div className="board_reset_btn">
-          <button><MarkType name='refresh' fill='#dad8d8' width='30px' /></button>
-        </div>
+      <div className="board_mark_turn">
+        <MarkType name='cross' width='30%' />
+        <MarkType name='circle' width='30%' />
+      </div>
+      <div className="board_turn_btn">
+        <MarkType name={turnValue} fill='#dad8d8' width='20px' />
+        <span>Turn</span>
+      </div>
+      <div className="board_reset_btn">
+        <button><MarkType name='refresh' fill='#192A32' width='30px' /></button>
       </div>
       <div className="board_slot_container">
         {[...Array(9)].map((e, index) =>
-          <div className="board_slot" key={index}>
-            <MarkType name='cross' width='60%' />
-          </div>
+          <Slot key={index} keyIndex={index} turnMark={turnValue} changeMark={handleClick} />
         )}
       </div>
-      <div className="board_footer">
-        <div className="board_score___your"></div>
-        <div className="board_score___draw"></div>
-        <div className="board_score___opponent"></div>
+      <div className="board_score___your">
+        <MarkType name={isCross ? "cross" : "circle"} width='20px' height='20px' fill='#192A32' className='board_score___icon' />
+        <span>(You)</span>
+        <p>11</p>
+      </div>
+      <div className="board_score___draw">
+        <span>Ties</span>
+        <p>value</p>
+      </div>
+      <div className="board_score___opponent">
+        <MarkType name={isCross ? "circle" : "cross"} width='20px' height='20px' fill='#192A32' className='board_score___icon' />
+        <span>(CPU)</span>
+        <p>value</p>
       </div>
     </div>
   )
