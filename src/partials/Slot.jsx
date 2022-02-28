4440208ff684589e888d70f6e613b92cd783f1b6
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-01 00:36:23 +0530

    Updated Slot Component

--- src/partials/Slot.jsx
+++ src/partials/Slot.jsx
@@ -1,21 +1,24 @@
 import { useState } from 'react';
 import MarkType from '../components/MarkType';
 
-const Slot = ({ keyIndex, turnMark, changeMark }) => {
+const Slot = ({ keyIndex, turnMark, changeMark, winState }) => {
 
     const [turnValue, setTurnValue] = useState("");
-    
     const [disable, setDisable] = useState(false);
+
+    const { winner, line } = winState;
+
+    const isWon = winner && line.includes(keyIndex);
 
     const handleClick = () => {
         setTurnValue(turnMark);
         setDisable(true);
-        changeMark();
+        changeMark(keyIndex);
     }
 
     return (
-        <button className="board_slot" key={keyIndex} onClick={handleClick} disabled={disable}>
-            <MarkType name={turnValue} width='60%' />
+        <button className={`board_slot ${isWon ? (winner==="cross"? "win___cross" : "win___circle") : "default"}`} key={keyIndex} onClick={handleClick} disabled={disable}>
+            <MarkType name={turnValue} fill={isWon ? "#192A32" : ""} width='60%' />
         </button>
     )
 }
