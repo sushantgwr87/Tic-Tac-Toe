957a1adbf8f0d02e7b2f785fcf8c3b31cc5fd31e
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-06 23:42:09 +0530

    Updated useWinner Hook

--- src/customHook/useWinner.js
+++ src/customHook/useWinner.js
@@ -1,31 +1,37 @@
-export default function useWinner(squares) {
+export default function useWinner(squares, mark) {
 
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        // return lines[i];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (squares[a] === mark) {
         return {
-            winner: squares[a],
-            line: lines[i]
+          isWon: true,
+          winner: squares[a],
+          line: lines[i]
         }
       }
       else {
-          return {
-              draw: true
-          }
+        return {
+          isLoss: true,
+          winner: squares[a],
+          line: lines[i]
+        }
       }
     }
+  }
 
-    return {};
+  return {
+    result: "draw",
+  };
 }
