98eedea3a470d93625b1eed215cd8b2b1373d75e
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-06 23:41:55 +0530

    Added Score Component

---
+++ src/components/Score.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import MarkType from './MarkType';
+
+const Score = ({ mark, gameStats }) => {
+
+    const isCross = mark === "cross";
+
+    const [scoreData, setScoreData] = useState({
+        win: 0,
+        draw: 0,
+        loss: 0 //Opponent's score
+    })
+
+    const { win, draw, loss } = scoreData;
+
+    useEffect(() => {
+        if (gameStats) {
+            if (gameStats.isWon)
+                setScoreData({ ...scoreData, win: win + 1 });
+            else if (gameStats.isLoss)
+                setScoreData({ ...scoreData, loss: loss + 1 });
+            else
+                setScoreData({ ...scoreData, draw: draw + 1 });
+        }
+    }, [])
+
+    return (
+        <>
+            <div className="board_score___your">
+                <MarkType name={isCross ? "cross" : "circle"} width='20px' height='20px' fill='#192A32' className='board_score___icon' />
+                <span>(You)</span>
+                <p>{win}</p>
+            </div>
+            <div className="board_score___draw">
+                <span>Ties</span>
+                <p>{draw}</p>
+            </div>
+            <div className="board_score___opponent">
+                <MarkType name={isCross ? "circle" : "cross"} width='20px' height='20px' fill='#192A32' className='board_score___icon' />
+                <span>(CPU)</span>
+                <p>{loss}</p>
+            </div>
+        </>
+    )
+}
+
+export default Score
