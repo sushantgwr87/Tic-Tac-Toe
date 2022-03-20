22257ad3facef5c2fd22224e8fbe36ea8f62ff2c
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-21 00:28:14 +0530

    Updated Slot Component

--- src/partials/Slot.jsx
+++ src/partials/Slot.jsx
@@ -1,13 +1,10 @@
-import { isDisabled } from '@testing-library/user-event/dist/utils';
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import MarkType from '../components/MarkType';
 
 const Slot = ({ keyIndex, turnMark, changeMark, isDisabled, winState }) => {
 
     const [turnValue, setTurnValue] = useState("");
     const [disable, setDisable] = useState(false);
-
-    // console.log(winState);
 
     const { winner, line } = winState;
 
@@ -19,21 +16,11 @@ const Slot = ({ keyIndex, turnMark, changeMark, isDisabled, winState }) => {
         changeMark(keyIndex);
     }
 
-    // useEffect(() => {
-    //     if (isWon)
-    //         setDisable(true);
-    // }, [])
-
-
     return (
         <>
             <button className={`board_slot ${isWon ? (winner === "cross" ? "win___cross" : "win___circle") : "default"}`} key={keyIndex} onClick={handleClick} disabled={disable || isDisabled}>
                 <MarkType name={turnValue} fill={isWon ? "#192A32" : ""} width='60%' />
             </button>
-            {/* <input type="hidden" id="slot" name="slot" disabled={disable} onClick={handleClick} />
-            <label className={`board_slot ${isWon ? (winner === "cross" ? "win___cross" : "win___circle") : "default"}`} key={keyIndex} onClick={handleClick}>
-                <MarkType name={turnValue} fill={isWon ? "#192A32" : ""} width='60%' />
-            </label> */}
         </>
     )
 }
