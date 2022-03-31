0c62c4c3f2749edbb9a6ac04b07c79229147ed54
sushantgwr87 <sushantgangwar87@gmail.com>
2022-03-31 22:32:42 +0530

    Updated App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import { useState } from "react";
+// import { useEffect } from "react";
 import useLocalStorage from "./customHook/useLocalStorage";
 import Board from "./components/Board";
 import Intro from "./components/Intro";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mark, setMark] = useLocalStorage("mark","cross");
-  const [mode, setMode] = useLocalStorage("mode",null);
+  const [mark, setMark] = useLocalStorage("mark", "cross");
+  const [mode, setMode] = useLocalStorage("mode", null);
   // const [mark, setMark] = useState("cross");
   // const [mode, setMode] = useState(null);
 
