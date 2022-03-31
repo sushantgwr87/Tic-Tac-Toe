b15b71d9f7fdd921259e262b5fe8fa0d762b14b8
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-01 00:21:39 +0530

    Changed local to Session storage hook

--- src/customHook/useSessionStorage.js
+++ src/customHook/useSessionStorage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const useLocalStorage = (key, initialValue) => {
+const useSessionStorage = (key, initialValue) => {
     // State to store our value
     // Pass initial state function to useState so logic is only executed once
     const [storedValue, setStoredValue] = useState(() => {
@@ -9,7 +9,7 @@ const useLocalStorage = (key, initialValue) => {
         }
         try {
             // Get from local storage by key
-            const item = window.localStorage.getItem(key);
+            const item = window.sessionStorage.getItem(key);
             // Parse stored json or if none return initialValue
             return item ? JSON.parse(item) : initialValue;
         } catch (error) {
@@ -19,7 +19,7 @@ const useLocalStorage = (key, initialValue) => {
         }
     });
     // Return a wrapped version of useState's setter function that ...
-    // ... persists the new value to localStorage.
+    // ... persists the new value to sessionStorage.
     const setValue = (value) => {
         try {
             // Allow value to be a function so we have same API as useState
@@ -29,7 +29,7 @@ const useLocalStorage = (key, initialValue) => {
             setStoredValue(valueToStore);
             // Save to local storage
             if (typeof window !== "undefined") {
-                window.localStorage.setItem(key, JSON.stringify(valueToStore));
+                window.sessionStorage.setItem(key, JSON.stringify(valueToStore));
             }
         } catch (error) {
             // A more advanced implementation would handle the error case
@@ -39,4 +39,4 @@ const useLocalStorage = (key, initialValue) => {
     return [storedValue, setValue];
 }
 
-export default useLocalStorage
+export default useSessionStorage
